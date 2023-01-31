872890f9b666a7251863235040fb921e5d1a720c
Miguel <miguel.bmangelulloa13@gmail.com>
2023-01-30 22:43:58 -0600

    Clase 1

---
+++ AprendiendoSwift/Mis PlayGrounds/Clase 1.playground/Contents.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+//Ejemplo de poner un comentario
+
+var variable = "Hola mundo"     //Crear una variable
+variable = "Hola mundo2"
+
+let constante = "Adios mundo"   //Crear una constante
+//constante = "Adios mundo2"    //No se puede modificar una constante
+
+//Las variables y constantes sirven para almacenar datos (guardar en memoria)
+
+//************************************************************************************************
+
+//Declaracion multiple de variables
+
+var num1 = 1, miNombre = "Miguel"
+let num2 = 2, miApellido = "Martinez"
+
+//************************************************************************************************
+
+//Tipos de datos basicos
+
+var x = 18      //Variable de tipo entero
+var y = 9.5     //Varible de tipo flotante (NUmero decimal)
+var mensaje = "Este es una variable String"     //Variable de tipo String
+let booleano = true     //Varaible de tipo bool (true or false)
+
+//************************************************************************************************
+
+//Operadores Aritmeticos
+
+var sumaEnteros = x + 2
+var restaEnteros = y + 1.7
+
+var sumar = Double(x) + Double(y)
+
+var division = x / 3
+
+var division2 = x / 5   //No se obtiene todo el resultado
+//Para obtener el resultado en numero decimal es necesario especificar el tipo de dato
+
+var division3 = Double(x) / 5
+
+//************************************************************************************************
+
+//Asignacion Explicita e Implicita
+
+var a1:Int = 200     //Asignacion explicita de una variable tipo entero (de 64 bits)
+var a2 = 200         //Asignacion IMPLICITA de una variable tipo entero
+
+var b1: String = "Este es un mensaje"
+var b2 = "Este es un mensaje"
+
+let c1: Float = 3.1416
+let c2 = 3.1416
+
+
+var d2: Bool = false
+var d1 = true
+
+
+
+
+
+
+
