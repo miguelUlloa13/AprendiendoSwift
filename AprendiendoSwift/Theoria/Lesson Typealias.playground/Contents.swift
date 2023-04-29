af36864258f9ed4838e8f90103a845013e8bb2a3
Miguel Ulloa <miguel.bmangelulloa13@gmail.com>
2023-04-28 19:05:16 -0600

    Agregando typealias y typecasting

--- AprendiendoSwift/Theoria/Lesson Typealias.playground/Contents.swift
+++ AprendiendoSwift/Theoria/Lesson Typealias.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-var greeting = "Hello, playground"
+    // MARK: - typealias
+        // Un typealias nos permite renombrar clases ya existentes.
+
+typealias Decimal = Float
+let miNumero: Decimal = 3.1416
+
+typealias Entero = Int
+let otroNumero: Entero = 5
+
+print(type(of: miNumero))   // el metodo type(of: ) sirve para obtener cual tipo es la variable
